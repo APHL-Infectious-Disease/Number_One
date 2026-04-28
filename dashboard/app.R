997a054ec6bec59280104c0cc624062664694be1
Hanley <64234987+HanleyKingston@users.noreply.github.com>
2026-04-28 16:44:06 +0000

    make basic barplot in dashboard from Kraken summary

--- dashboard/app.R
+++ dashboard/app.R
@@ -9,59 +9,72 @@
 
 library(shiny)
 library(htmltools)
-# Define UI for application that draws a histogram
+library(tidyverse)
+
+# Define UI for application that draws a barplot
 ui <- fluidPage(
-
-    # Application title
-    titlePanel("Old Faithful Geyser Data"),
-
-    #UI input - sliders (date), button, arranging. - V 
-
-    # Sidebar with a slider input for number of bins 
-    sidebarLayout(
-        sidebarPanel(
-            sliderInput("bins",
-                        "Number of bins:",
-                        min = 1,
-                        max = 50,
-                        value = 30)
-        ),
-
-        # Show a plot of the generated distribution
-        mainPanel(
-           plotOutput("distPlot")
-        )
+  
+  # Application title
+  titlePanel("Enteric Virus Reads"),
+  
+  #UI input - sliders (date), button, arranging. - V 
+  
+  # Sidebar with a slider input for number of bins 
+  sidebarLayout(
+    sidebarPanel(
+      selectInput("fill_var",
+                  "Color By:",
+                  choices = c("scientific_name", "sample_srx", "year"))
+    ),
+    
+    # Show a plot of the generated distribution
+    mainPanel(
+      plotOutput("barPlot")
     )
+  )
 )
 
 # Define server logic required to draw a histogram
 server <- function(input, output) {
-
-    output$distPlot <- renderPlot({
-        # generate bins based on input$bins from ui.R
-        x    <- faithful[, 2]
-        bins <- seq(min(x), max(x), length.out = input$bins + 1)
-
-        # draw the histogram with the specified number of bins
-        hist(x, breaks = bins, col = 'darkgray', border = 'white',
-             xlab = 'Waiting time to next eruption (in mins)',
-             main = 'Histogram of waiting times')
-    })
-
-#import data and transform - Script from Hanley
-# tabulated data - data table on a tab
-# ggplot - taxon breakdown by state AND National, and frequency
-# ggplot - time bound
-#
-
-
-
-
-# mapping - Leaflet map. V 
-# state shading? organism
-
-
-
+  
+  # Load data
+  kraken_summary <- read.csv("results/summary/kraken_summary-2026-04-28.csv")
+  
+  output$barPlot <- renderPlot({
+    
+    # Parse input values (to deal with non-standard ggplot aes nomenclature)
+    #color_by <- input$color_by
+    
+    # generate bins based on input$bins from ui.R
+    #bins <- seq(min(x), max(x), length.out = input$bins + 1)
+    
+    # draw the histogram with the specified number of bins
+    kraken_summary %>%
+      # Drop unclassified from results
+      mutate(year = year(as.Date(summary_date))) %>% #using summary date just to test, but this needs to be changed to the collection date from the metadata
+      filter(scientific_name != "unclassified") %>%
+      ggplot() +
+      geom_bar(aes_string(x = "sample_srx", y="count", fill = input$fill_var), stat = "identity") +
+      theme(axis.text.x = element_text(angle = 45, hjust = 1)) +
+      labs(
+        y = "read count"
+      )
+  })
+  
+  #import data and transform - Script from Hanley
+  # tabulated data - data table on a tab
+  # ggplot - taxon breakdown by state AND National, and frequency
+  # ggplot - time bound
+  #
+  
+  
+  
+  
+  # mapping - Leaflet map. V 
+  # state shading? organism
+  
+  
+  
 }
 
 #save html
